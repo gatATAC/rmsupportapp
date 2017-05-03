f1e9ef7ac7a9c6ea4fcf83bf68a7f89081e1fef5
Txinto Vaz <txinto@.iac.es>
2017-05-03 09:37:59 +0100

    solve sub-URI deployment issues for issue context help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :action_set_url_options
+ 
+   def action_set_url_options
+     if ENV['RAILS_RELATIVE_URL_ROOT']
+       @host = request.host+":"+request.port.to_s+"/"+ENV['RAILS_RELATIVE_URL_ROOT']
+     else
+       @host = request.host+":"+request.port.to_s
+     end
+     Rails.application.routes.default_url_options = { :host => @host}
+   end  
+
+  def self.url_root
+      if ENV['RAILS_RELATIVE_URL_ROOT']
+        return "/"+ENV['RAILS_RELATIVE_URL_ROOT']
+      else
+        return ""
+      end
+  end
 end
